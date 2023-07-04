2389901cbaf2fef5a2dbd49d5083657c6bfd5fc8
Pierluigi Salera <igi_sweden@yahoo.it>
2023-07-04 07:40:08 +0200

    adding more exercises in documento26 folder

---
+++ exercises/documento26/AccessTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE NomiPersone (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(50),
+    Cognome VARCHAR(50),
+    DataNascita DATE,
+    Indirizzo VARCHAR(100),
+    Città VARCHAR(50),
+    CAP VARCHAR(10)
+);
